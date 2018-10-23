abb1490d443529f3763420e92f94ed25fd5e47ff
小工匠 <yangshangwei@users.noreply.github.com>
2018-10-23 10:53:25 +0800

    Initial commit

---
+++ src/test/java/com/artisan/springbootmaster/SpringbootmasterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.artisan.springbootmaster;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootmasterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
